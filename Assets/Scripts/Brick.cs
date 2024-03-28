03e15e9eb90c0e159b9270eca2c2162e156a063f
pijussJ <pijus.jarusevicius3@gmail.com>
2024-03-28 17:47:28 +0200

    Particleai

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     [SerializeField]private int hp;
+    public ParticleSystem ps;
 
     public void Damage()
     {
         hp--;
         if (hp <= 0)
         {
+            ps.Play();
             Destroy(gameObject);
         }
     }
